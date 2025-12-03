bb41237ab6597df5593c292cee4546a28445367f
nina-niederberger <n1n4_fg@proton.me>
2025-12-03 12:26:52 +0100

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-naechste",
+    prevEl: ".swiper-button-vorherige",
+  },
+
+});
