7888807d898f91e258b77cd3083dbe7e3713e478
nina-niederberger <n1n4_fg@proton.me>
2025-12-07 17:14:59 +0100

    hauptseite

--- script.js
+++ script.js
@@ -10,23 +10,4 @@ const swiper = new Swiper(".swiper", {
 
 });
 
-.swiper {
-  width: 100%;
-  height: 85vh; /* Beispiel: Nutze 85% der Bildschirmhöhe */
-}
 
-/* Wichtig: Sorge dafür, dass die Bilder den gesamten verfügbaren Platz im Slide einnehmen */
-.swiper-slide {
-  /* Zentriert den Inhalt vertikal */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  /* Das Bild deckt den Bereich ab, ohne die Proportionen zu verzerren */
-  object-fit: cover; 
-}
