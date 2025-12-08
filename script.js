714ce1152e0ac60eb4483a3c3b975123c753c7a0
nina-niederberger <n1n4_fg@proton.me>
2025-12-08 17:32:31 +0100

    verbesserung startseite

--- script.js
+++ script.js
@@ -11,24 +11,3 @@ const swiper = new Swiper(".swiper", {
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-naechste',
-            prevEl: '.swiper-button-vorherige',
-        },
-        
-        // HIER IST DIE PAGINIERUNG
-        pagination: {
-            el: '.swiper-pagination', // Wähle den Container, den du im HTML erstellt hast
-            clickable: true,          // Erlaubt das Klicken auf die Punkte, um direkt zum Slide zu springen
-        },
-        
-        // Füge hier deine weiteren Einstellungen (wie slidesPerView) hinzu
-    });
-});
